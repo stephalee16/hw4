b87bb5b3289ddb16c5edc899a3f7e13b93ba567b
Stephanie Lee <stephanie.lee1@kellogg.northwestern.edu>
2023-06-03 10:08:26 +0000

    user authorization updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,7 @@ class PostsController < ApplicationController
       @post["description"] = params["post"]["description"]
       @post["posted_on"] = params["post"]["posted_on"]
       @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = session["user_id"]
       @post.save
     else
       flash["notice"]="Login first"
